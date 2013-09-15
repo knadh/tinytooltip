6f7530795408f9ae264d085e1d1d270b5332ca13
Kailash Nadh <kailash.nadh@gmail.com>
2013-09-15 13:49:25 +0530

    Refactored a bit

--- src/jquery.tinytooltip.js
+++ src/jquery.tinytooltip.js
@@ -1,9 +1,10 @@
 /*
 	Kailash Nadh (http://kailashnadh.name)
 
-	tinytooltip
-	March 2012
+	tinytooltip | March 2012
 	A tiny tooltip plugin for jQuery
+
+	Refactored on 15 September 2013
 
 	License	:	MIT License
 */
@@ -20,39 +21,50 @@
 		if(args) {
 			$.extend(options, args);
 		}
-		
+
 		return this.each(function(){
 			var me = $(this);
-			if(!me.width()) return false;
 
+			me.bind( (options.hover ? 'mouseover ' : '') + 'showtooltip', function() {
+				render(me);
+			});
+			me.bind( (options.hover ? 'mouseout ' : '') + 'hidetooltip', function() {
+				destroy(me);
+			});
+		});
+
+		function render(me) {
 			// parent's position
 			var pos = me.offset();
 			
-			// silly hack to calculate the message's width
-			var dummy = $('<span class="tooltip_dummy">' + options.message + '</span>').css('visibility', 'hidden');
-			$('body').append(dummy);
-			var message_width = dummy.width();
-			dummy.remove();
-
 			// tooltip html
-			var tip = $('<span class="tinytooltip'+ (options.classes ? ' ' + options.classes : '') +'"><span class="arrow"></span><span class="message">' + options.message + '</span></span>');
+			var tip = $('<span class="tinytooltip'+ (options.classes ? ' ' + options.classes : '') +'">');
+				tip.append('<span class="arrow">');
+				tip.append(
+					$('<span class="message">').append( typeof options.message == "function" ? options.message.call(me, tip) : options.message )
+				);
 
-			tip.width(message_width).css('opacity', 0).hide();
+			tip.css('opacity', 0).hide();
 			$('body').append(tip);
 
 			// position the tooltip beside the parent
 			tip.css('left', pos.left + (me.outerWidth()/2) - (tip.outerWidth()/2));
 			tip.css('top', pos.top + me.outerHeight());
 
-			me.bind( (options.hover ? 'mouseover ' : '') + 'showtooltip', function() {
-				tip.show().animate({opacity: 1}, 200);
-			});
-			me.bind( (options.hover ? 'mouseout ' : '') + 'hidetooltip', function() {
+			me.data('tinytooltip', tip);
+
+			tip.show().animate({opacity: 1}, 200);
+		}
+
+		function destroy(me) {
+			var tip = me.data('tinytooltip');
+
+			if(tip) {
 				tip.animate({opacity: 0}, 200, function() {
-					$(this).hide();
+					$(this).remove();
 				});
-			});
-		});
+			}
+		}
 	};
 	
 	return this;
